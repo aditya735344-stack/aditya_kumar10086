e725944b806175a073019c7d7a96a124180784dd
aditya735344-stack <aditya735344@gmail.com>
2025-12-30 20:25:43 +0800

    Add files via upload

---
+++ practical8/alphabatepattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    //Name-Aditya kumar ERPID -10086
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    for(int i = 1; i <= n; i++) {
+        char ch = 'A';
+        for(int j = 1; j <= i; j++) {
+            printf("%c", ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//Output
+// Enter n:5
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
